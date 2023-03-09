74ecff8ee00be12579d0c50f1e5fd6e7eddfc201
YUNJI <78493326+dlqmsdbswl00@users.noreply.github.com>
2023-03-09 15:59:22 +0900

    Add files via upload
    3/9

---
+++ 005_helloworld/Form1.Designer.cs
@@ -0,0 +1,65 @@
+﻿namespace _005_helloworld
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblHW = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lblHW
+            // 
+            this.lblHW.AutoSize = true;
+            this.lblHW.Font = new System.Drawing.Font("맑은 고딕", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.lblHW.Location = new System.Drawing.Point(280, 187);
+            this.lblHW.Name = "lblHW";
+            this.lblHW.Size = new System.Drawing.Size(221, 50);
+            this.lblHW.TabIndex = 0;
+            this.lblHW.Text = "HelloWorld";
+            this.lblHW.Click += new System.EventHandler(this.lblHW_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lblHW);
+            this.Name = "Form1";
+            this.Text = "HelloWorld";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblHW;
+    }
+}
+
---
+++ 005_helloworld/Form1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _005_helloworld
+{
+    public partial class Form1 : Form
+    {
+        Color defaultFormColor;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            defaultFormColor = this.BackColor;
+        }
+
+        private void lblHW_Click(object sender, EventArgs e)
+        {
+            if (this.BackColor == defaultFormColor)
+            {
+                this.BackColor = Color.Tomato;
+                lblHW.ForeColor = Color.White;
+            }
+            else
+            {
+                this.BackColor = defaultFormColor;
+                lblHW.ForeColor = Color.Black;
+            }
+        }
+
+        
+    }
+}
